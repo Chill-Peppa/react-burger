f47f984f6b08aa1630ebb4872c4d2ac632d43e35
Anastasia <www.nastya97@yandex.ru>
2023-11-16 20:58:50 +0300

    feat: api types

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -10,18 +10,18 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { login } from '../../services/actions/auth';
 
-function Login({ title }) {
+function Login({ title }: { title: string }) {
   const [user, setUser] = React.useState({ email: '', password: '' });
 
   const dispatch = useDispatch();
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch(login(user));
+    dispatch<any>(login(user));
     console.log(user);
   };
 
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
 // состояние стейтов для табов
-export const tabs = {
-  BUN: 'bun',
-  SAUCE: 'sauce',
-  MAIN: 'main',
-};
+export enum tabs {
+  BUN = 'bun',
+  SAUCE = 'sauce',
+  MAIN = 'main',
+}
 
 export const BASE_URL = 'https://norma.nomoreparties.space';
 
